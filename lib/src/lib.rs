3169587db09d07d945326062e17b42bd3caa8aba
gcxfd <i@rmw.link>
2022-03-01 19:05:09 +0800

    🔶

--- lib/src/lib.rs
+++ lib/src/lib.rs
@@ -1,8 +1,12 @@
+pub fn test() {
+  println!("hi")
+}
+
 #[cfg(test)]
 mod tests {
-    #[test]
-    fn it_works() {
-        let result = 2 + 2;
-        assert_eq!(result, 4);
-    }
+  #[test]
+  fn it_works() {
+    let result = 2 + 2;
+    assert_eq!(result, 4);
+  }
 }
