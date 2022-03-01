a8dbbbf6796368f1a2299309557f173755131e16
gcxfd <i@rmw.link>
2022-03-01 19:13:14 +0800

    🔶

---
+++ lib/dist.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+DIR=$(dirname $(realpath "$0"))
+cd $DIR
+set -ex
+
+ cargo +nightly build -Z build-std=std,panic_abort \
+   -Z build-std-features=panic_immediate_abort \
+  --target x86_64-apple-darwin --release
